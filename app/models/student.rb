2e3f24a06f40b55a52946e53d821b0a448b9d772
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-24 13:06:03 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ActiveRecord::Base
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE student (
+      id INTEGER PRIMARY KEY,
+        name TEXT
+        )
+      SQL
+
+     ActiveRecord::Base.connection.execute(sql)
+  end
 end
